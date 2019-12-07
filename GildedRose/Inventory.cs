3556a99c9d1279f2b9e0b32966276bc934f28ca6
romankalab <roan.kalab@mail.com>
2019-12-07 22:05:02 +0100

    Initial

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -24,14 +24,34 @@ namespace GildedRose
         public void UpdateQuality()
         {
             int qdecrease = 1;
+            int qincrease = 1;
+            int originalq = 0;
             foreach (Item item in items)
             {
+                originalq = item.Quality;
                 item.SellIn = item.SellIn - 1;
                 if (item.SellIn < 0)
                     qdecrease = qdecrease * 2;
 
                 if (item.Name.Contains("Conjured"))
                     qdecrease = qdecrease * 2;
+
+
+                item.Quality = item.Quality - qdecrease;
+
+                if (item.Name.Contains("Aged Brie"))
+                {
+                    item.Quality = originalq;
+                    item.Quality = item.Quality + qincrease;
+                }
+
+                if (item.Quality < 0)
+                    item.Quality = 0;
+                if (item.Quality > 50)
+                    item.Quality = 50;
+
+                if (item.Name.Contains("Sulfuras"))
+                    item.Quality = originalq;
             }
         }
     }
